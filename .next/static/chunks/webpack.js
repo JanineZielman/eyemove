67af25f4a994db285b5674927abf6f42e9c91b45
JanineZielman <janine.zielman@icloud.com>
2021-12-04 14:30:30 +0100

    small adjustment

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "7b70738b70cc2fe5"; }
+/******/ 		__webpack_require__.h = function() { return "833e53998cc41226"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
